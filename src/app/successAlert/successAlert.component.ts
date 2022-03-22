1b75f81e6dec7ff71fd382b782e39f7a3ee00d6a
hoanplpyy <hoanplpyy@hotmail.com>
2022-03-22 09:13:45 +0800

    directives

--- src/app/successAlert/successAlert.component.ts
+++ src/app/successAlert/successAlert.component.ts
@@ -1,14 +1,30 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+
 
 
 @Component({
-  selector:'app-successalert',
-  templateUrl:'./successalert.component.html',
-  styleUrls:['./successalert.component.css']
+  selector: 'app-successalert',
+  templateUrl: './successalert.component.html',
+  styleUrls: ['./successalert.component.css']
 })
 
-export class SuccessAlertComponent{
+export class SuccessAlertComponent implements OnInit {
 
+  serverName: string;
+  serverCreated: boolean = false;
+  serverCreateStatus = "Server is not created"
+  allowNewServer = false;
 
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000)
+
+  }
+
+  onCreateServer() {
+    this.serverCreateStatus = "Server is created";
+    this.serverCreated = true;
+  }
 
 }
