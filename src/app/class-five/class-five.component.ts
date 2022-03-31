0070ba153b300d0da5326743e68348e977f06549
hoanplpyy <hoanplpyy@hotmail.com>
2022-03-31 13:25:35 +0800

    dom fromPC 13:25

--- src/app/class-five/class-five.component.ts
+++ src/app/class-five/class-five.component.ts
@@ -1,17 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-class-five',
   templateUrl: './class-five.component.html',
-  styleUrls: ['./class-five.component.css']
+  styleUrls: ['./class-five.component.css'],
+//  encapsulation:ViewEncapsulation.ShadowDom
 })
 export class ClassFiveComponent implements OnInit {
 
-  serverElements=[{type:'server',name:'testServer',content:'just test'}]
+  serverElements = [{ type: 'server', name: 'testServer', content: 'just test' }]
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+
+  onServerAdded(serverData: { serverName: string, serverContent: string }) {
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.serverName,
+      content: serverData.serverContent
+    });
+  }
+
+  onBlueprintAdded(blueprintData: { serverName: string, serverContent: string }) {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: blueprintData.serverName,
+      content: blueprintData.serverContent
+    });
   }
 
 }
